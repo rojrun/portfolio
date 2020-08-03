f523f5b759edae021b37e69ef57f81563d054df2
rojrun <runroj@gmail.com>
2020-08-02 20:40:30 -0700

    added composer and phpmailer

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,4 +1,17 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+require '../vendor/autoload.php';
+$mail = new PHPMailer;
+$mail->isSMTP();
+$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+$mail->Host = 'smtp.gmail.com';
+$mail->Port = 587;
+$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+$mail->SMTPAuth = true;
+$mail->Username = $secret_email;
+$mail->Password = $secret_password;
+
 // Check for empty fields
 if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
@@ -7,33 +20,27 @@ if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || 
 
 $name = strip_tags(htmlspecialchars($_POST['name']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
-$phone = phone_number_format(strip_tags(htmlspecialchars($_POST['phone'])));
+$phone = $_POST['phone'];
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
-function phone_number_format($number) {
-  $length = strlen($number);
-  if($length == 10) {
-    $number = preg_replace("/^1?(\d{3})(\d{3})(\d{4})$/", "$1-$2-$3", $number);
-  } 
-  return $number;
-}
-
 // Create the email and send the message
-$config = 'MIME-Version: 1.0';
-$config .= 'Content-type: text/html; charset=iso-8859-1';
-$to = "run_roj@hotmail.com"; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'runroj@gmail.com'; // Add your email address inbetween the "" replacing yourname@yourdomain.com - This is where the form will send a message to.
 $subject = "Website Contact Form:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email\n\nPhone: $phone\n\nMessage:\n$message";
 $header = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $header .= "Reply-To: $email";	
 
-if(!mail($to, $subject, $body, $header, $config)) {
+$mail->setFrom($email, $name);
+$mail->addAddress($secret_email, 'Roj Rungsisullatanont');
+$mail->Subject = $subject;
+// $mail->addAttachment('images/phpmailer_mini.png');
+
+if(!mail($to, $subject, $body, $header)) {
   var_dump( error_get_last());
   var_dump($to);
-  var_dump($subject);
-  var_dump($body);
-  var_dumP($header);
-  var_dump($config);
+  echo var_dump($subject);
+  echo var_dump($body);
+  echo var_dumP($header);
   http_response_code(500);
 }  
 ?>
